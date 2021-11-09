3a15ca64a2f17ebdf969691a9641f845e016b114
Baldbutgold <abdouplayz2003@gmail.com>
2021-11-09 14:48:14 +0100

    Basic Sound Board

---
+++ 50days/9/script.js
@@ -0,0 +1,24 @@
+function applause() {
+	var audio = new Audio('sounds/applause.mp3')
+	audio.play()
+}
+function boo() {
+	var boo = new Audio('sounds/boo.mp3')
+	boo.play()
+}
+function gasp() {
+	var gasp = new Audio('sounds/gasp.mp3')
+	gasp.play()
+}
+function tada() {
+	var tada = new Audio('sounds/tada.mp3')
+	tada.play()
+}
+function victory() {
+	var victory = new Audio('sounds/victory.mp3')
+	victory.play()
+}
+function wrong() {
+	var wrong = new Audio('sounds/wrong.mp3')
+	wrong.play()
+}
